2045f11752a5beb812735d5b0c6b30f79c18b6ef
copilot7766 <111620227+copilot7766@users.noreply.github.com>
2022-09-01 09:57:17 +0800

    Create ng.py

---
+++ ng.py
@@ -0,0 +1,9 @@
+from pyngrok import ngrok
+
+ngrok.set_auth_token('27xqdUZec8gJCpJ2g8maHKgQAuA_6uT52UntAv25GP48JzA4G')
+active_tunnels = ngrok.get_tunnels()
+for tunnel in active_tunnels:
+    public_url = tunnel.public_url
+    ngrok.disconnect(public_url)
+url = ngrok.connect(addr=10086, bind_tls=True)
+print(url.public_url + '/?token=123456')
